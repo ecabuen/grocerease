dedd09494895f4b96b1435a2624a773a22735fd5
Eloisa Cabuen <21-70803@g.batstate-u.edu.ph>
2024-12-01 18:43:38 +0800

    Add files via upload

---
+++ admin/riders.php
@@ -0,0 +1,7 @@
+<div id="header">
+        <?php include('admin-header.php'); ?>
+    </div>
+    <div id="main-wrapper">
+        <div id="sidebar">
+            <?php include('admin-sidebar.php'); ?>
+        </div>
